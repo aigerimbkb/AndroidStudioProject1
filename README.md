bdb0a2d1bd52060622543fed9ab72208d3915014
Aigerim Bekbolatova <aigerim0799@gmail.com>
2022-10-04 12:20:58 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# TSN_ANDROID_DEMO_HELLO
+Простейшая программа суммы двух цифр на Android
+
+![Screenshot](screenshot.png)
+
+https://www.youtube.com/watch?v=PMYmKEhbh0s
---
+++ TSN_DEMO_01_Sum/src/main/java/kz/talipovsn/sum/MainActivity.java
@@ -0,0 +1,63 @@
+package kz.talipovsn.sum;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText_a; // Переменная для доступа к компоненту со значением "a"
+    EditText editText_b;
+    EditText editText_x;// Переменная для доступа к компоненту со значением "b"
+    TextView textView_sum; // Переменная для доступа к компоненту со значением результата
+    Button buttonSum; // Переменная для доступа к компоненту кнопки
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Доступ к компонентам окна
+        editText_a = findViewById(R.id.editText_a);
+        editText_b = findViewById(R.id.editText_b);
+        editText_x = findViewById(R.id.editText_x);
+        textView_sum = findViewById(R.id.textView_sum);
+        buttonSum = findViewById(R.id.buttonSum);
+    }
+
+    // МЕТОД КНОПКИ РАСЧЕТА
+    public void onClick(View v) {
+        // Объявление локальных переменных
+        double a, b, x,y;
+
+        try { // НАЧАЛО ЗАЩИЩЕННОГО БЛОКА
+
+            // Чтение данных из компонент
+            a = Double.parseDouble(editText_a.getText().toString().trim());
+            b = Double.parseDouble(editText_b.getText().toString().trim());
+            x = Double.parseDouble(editText_x.getText().toString().trim());
+
+            // Основной алгоритм
+            if (x>=4) {
+                y = 12 * (x/((a*a*a)-b));
+            }
+            else {
+                y = 3 * Math.pow(x, 2) - a * b;
+            }
+
+            // Вывод полученного значения в компонент
+            textView_sum.setText(String.valueOf(y));
+
+        } catch (Exception e) { // ЧТО ДЕЛАТЬ ЕСЛИ ВОЗНИКНЕТ ОШИБКА В БЛОКЕ МЕЖДУ "TRY" И "CATCH":
+
+            // Вывод сообщения об ошибке
+            textView_sum.setText("Неверные входные данные для расчета!");
+
+        }  // КОНЕЦ ЗАЩИЩЕННОГО БЛОКА
+
+    }
+
+}
